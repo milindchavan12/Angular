124cb32a74ea6c4f6f0b38aa5d1de59663ae3109
Milind Chavan <milindchavan12@gmail.com>
2018-07-14 11:49:39 +0200

    Interfce

--- Sources/APM/src/app/products/product-list.component.ts
+++ Sources/APM/src/app/products/product-list.component.ts
@@ -1,11 +1,69 @@
-import { Component } from "@angular/core"
+import { Component, OnInit } from "@angular/core"
+import { IProduct } from "./product";
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent
- {
+export class ProductListComponent implements OnInit {
+
+    pageTitle: string = "Product List!";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
     
+    _listFilter: string;
+    
+    public get listFilter() : string {
+        return this._listFilter;
+    }  
+    public set listFilter(value : string) {
+        this._listFilter = value;
+        this.filteredProducts = this._listFilter ? this.performFilter(this._listFilter) : this.products;
+    }
+    
+    filteredProducts : IProduct[];
+    products: IProduct[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+        }
+    ];
+
+    constructor() {
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
+    }
+
+    ngOnInit(): void {
+        console.log('OnInit');
+    }
+
+    toggleShow() : void {
+        this.showImage = !this.showImage;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product : IProduct) =>
+            product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
 }
